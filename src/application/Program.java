c1ad97416c30ed3ea404eb92cb582ef09e1372b9
Lázaro <lazaronobrega3@gmail.com>
2022-12-12 18:34:43 -0300

    Criação entidade Seller e Department

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "Books");
+		Seller seller = new Seller(2, "joão", "joao@gmail.com", new Date(), 3000.0, dep);
+		
+		System.out.println(dep);
+		System.out.println(seller);
+		
+		
+	}
+
+}
